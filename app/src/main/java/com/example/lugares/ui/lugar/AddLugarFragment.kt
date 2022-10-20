c5fe4871deaa503c8fc8ed688d59e71c8579bddc
mrojasso <mrojas50105@ufide.ac.cr>
2022-10-19 20:47:15 -0600

    clase 3
    commits clase 3

---
+++ app/src/main/java/com/example/lugares/ui/lugar/AddLugarFragment.kt
@@ -0,0 +1,59 @@
+package com.example.lugares.ui.lugar
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import com.example.lugares.R
+import com.example.lugares.databinding.FragmentAddLugarBinding
+import com.example.lugares.viewmodel.LugarViewModel
+
+
+class AddLugarFragment : Fragment() {
+
+
+    private var _binding: FragmentAddLugarBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var lugarViewModel: LugarViewModel
+
+    override fun onCreate(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        lugarViewModel =
+            ViewModelProvider(this)[LugarViewModel::class.java]
+        _binding = FragmentAddLugarBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+}
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_lugar, container, false)
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment AddLugarFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            AddLugarFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
